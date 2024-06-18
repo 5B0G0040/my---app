5aeedbea3b5a4d0ad435b56cccc80ca0ca629288
5B0G0040 <5B0G0040@office.stust.edu.tw>
2024-06-18 23:03:33 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
+      </div>
+  );
+}
+
+export default App;
