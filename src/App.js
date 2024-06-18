2ef3e13330d43e11693f9bf3612f0f3217df6920
5B0G0040 <5B0G0040@office.stust.edu.tw>
2024-06-18 23:06:07 +0800

    my---app2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel"
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <MyRGBPanel />
       <h1>MyCalculator</h1>
       <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
       </div>
   );
 }
